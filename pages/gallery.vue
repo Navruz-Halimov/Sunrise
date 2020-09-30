5e9505236afad11e6431b11381c80389ca280dc7
Hojiakbar88 <hojiakbaregamberdiyev88@gmail.com>
2020-09-30 18:14:35 +0500

    Add carousel,modal,gallery

---
+++ pages/gallery.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <b-container>
+      <Test :thumbnail="images[0]" :images="images" />
+    </b-container>
+  </div>
+</template>
+<script>
+  import Test from '../components/Test.vue';
+export  default {
+  name:'app',
+  comments: {
+    Test,
+  },
+  data(){
+    return  {
+      images: [
+        'https://www.wyndhamhotels.com/content/dam/property-images/en-us/hr/uz/others/tashkent/49135/49135_exterior_view_1.jpg?crop=3000:2000;*,*&downsize=720:*',
+        'https://www.wyndhamhotels.com/content/dam/property-images/en-us/hr/uz/others/tashkent/49135/49135_exterior_view_2.jpg?crop=1278:852;*,*&downsize=720:*',
+        'https://www.wyndhamhotels.com/content/dam/property-images/en-us/hr/uz/others/tashkent/49135/49135_lobby_view_2.jpg?crop=3000:2000;*,*&downsize=720:*',
+        'https://www.wyndhamhotels.com/content/dam/property-images/en-us/hr/uz/others/tashkent/49135/49135_lobby_view_3.jpg?crop=3000:2000;*,*&downsize=720:*',
+      ],
+    }
+  },
+  methods:{
+
+  }
+}
+
+</script>
