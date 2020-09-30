eae743129d57fae43c62eeb522d61c9b7fb11465
Navruz <navruz.halimov.3999@gmail.com>
2020-09-30 14:27:16 +0500

    some changes

--- pages/amenties.vue
+++ pages/amenties.vue
@@ -3,7 +3,35 @@
     <b-container>
       <h1 class="text-center amenties__heading">Amenities</h1>
       <div class="amenties__inner">
-
+            <div class="amenties__header">
+              <h6>Most popular facilities</h6>
+              <ul>
+                <li>Free WiFi</li>
+                <li>Airport shuttle</li>
+                <li>Family rooms </li>
+                <li>Free parking</li>
+                <li>24-hour front desk</li>
+                <li>Non-smoking rooms</li>
+              </ul>
+            </div>
+        <div class="amenties__body">
+          <b-row>
+            <b-col md="4">
+              <ul class="outdoor__list">
+                <li class="outdoor__item">
+                  <span class="outdoor__item-icon"></span>
+                  <span class="outdoor__item-heading">Outdoors</span>
+                </li>
+                <li class="outdoor__item">Outdoor pool (all year)</li>
+                <li class="outdoor__item">Sun terrace</li>
+                <li class="outdoor__item">Outdoor pool</li>
+                <li class="outdoor__item">BBQ facilities</li>
+                <li class="outdoor__item">Terrace</li>
+                <li class="outdoor__item">Garden</li>
+              </ul>
+            </b-col>
+          </b-row>
+        </div>
       </div>
     </b-container>
   </div>
