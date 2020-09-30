371b1f19b7942210fe98e04b077f7efa08f7d507
Navruz <navruz.halimov.3999@gmail.com>
2020-09-30 12:54:41 +0500

    amenties page added

---
+++ pages/amenties.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="amenties">
+    <b-container>
+      <h1 class="text-center amenties__heading">Amenities</h1>
+      <div class="amenties__inner">
+
+      </div>
+    </b-container>
+  </div>
+</template>
