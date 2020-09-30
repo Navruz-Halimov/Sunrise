7c585ea93e12a89a814ba2cf090f4269998b0e51
Mirjalol9797 <miramirxomitov97@gmail.com>
2020-09-30 18:14:49 +0500

    Added contact file, contact style and events file, events style

---
+++ pages/contact.vue
@@ -0,0 +1,106 @@
+<template>
+    <div class="contact-page">
+        <b-container>
+            <b-row>
+                <b-col sm="12">
+                    <h1 class="contact-page__heading">
+                        Contact information
+                    </h1>
+                    <div class="contact-page-details">
+                        <b-container>
+                            <b-row>
+                                <b-col lg="6">
+                                    <div class="contact-page-details__cards">
+                                        <h3 class="contact-page-details__title">General contact</h3>
+                                        <div class="contact-page-details__phone">
+                                            <font-awesome-icon :icon="['fas', 'phone-alt']" />
+                                            <a href="tel:+998 78 1204900">+998 78 1204900</a>
+                                        </div>
+                                        <div class="contact-page-details__email">
+                                            <font-awesome-icon :icon="['fas', 'envelope']" />
+                                            <a href="mailto:reservation.tashkent@radissonblu.com">
+                                                reservation.tashkent@radissonblu.com
+                                            </a>
+                                        </div>
+                                        <div class="contact-page-details__fax">
+                                            <font-awesome-icon :icon="['fas', 'fax']" />
+                                            <a href="tel:+998 78 1204900 ">+998 78 1204900</a>
+                                        </div>
+                                    </div>
+                                </b-col>
+                            </b-row>
+                        </b-container>
+                    </div>
+                    <div class="contact-page-info">
+                        <b-container>
+                            <b-row>
+                                <b-col cols="12">
+                                    <h2 class="contact-page-info__title">How to get to the Radisson Blu Hotel, Tashkent</h2>
+                                </b-col>
+                                <b-col cols="12">
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore expedita natus autem, consequatur earum neque?
+                                </b-col>
+                            </b-row>   
+                        </b-container>
+                    </div>                    
+                </b-col>
+            </b-row>
+        </b-container>
+        <div class="contact-page-map">
+            <b-container fluid>
+                <b-row></b-row>
+            </b-container>
+        </div>
+        <div class="contact-page-btn">
+            <b-container>
+                <b-row>
+                    <b-col cols="12">
+                        <button class="contact-page-btn__button">
+                            <font-awesome-icon :icon="['fas', 'map-marker-alt']" />
+                            <strong>How to arrive at Amir Temur Street 88 Tashkent 100084 Uzbekistan</strong>
+                            <font-awesome-icon :icon="['fas', 'times']" class="closeButton" />
+                        </button>
+                    </b-col>
+                </b-row>
+                <b-row>
+                    <b-form inline class="contact-page-btn__form">
+                        <b-col lg="3" class="p-0">
+                            <b-form-group>
+                                <!-- <label for="input" class=""></label> -->
+                                <b-input id="input" placeholder="Where are you starting from?" class="form-control"></b-input>
+                            </b-form-group>
+                        </b-col>
+                        <b-col lg="6" xl="5" offset-xl="1" class="d-flex justify-content-between mb-0 p-0 form-group" >
+                            <label for="" class="contact-page-btn__form-activ">
+                                <font-awesome-icon :icon="['fas', 'car']" />
+                            </label>
+                            <label for="">
+                                <font-awesome-icon :icon="['fas', 'bus']" />
+                            </label>
+                            <label for="">
+                                    <font-awesome-icon :icon="['fas', 'walking']" />
+                            </label>
+                        </b-col>
+                        <b-col lg="3" xl="2" offset-xl="1" class="p-0">
+                            <b-form-group>
+                                <b-button type="submit" class="contact-page-btn__get">Get directions</b-button>
+                            </b-form-group>
+                        </b-col>
+                    </b-form>
+                </b-row>
+            </b-container>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            
+        },
+        methods: {
+            openBlock: function() {
+                contactMapBtn.classList.toggle('button-show');
+            }
+        }
+    }
+</script>
---
+++ pages/events.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="event-page">
+        <b-container>
+            <div class="event-page__top">
+                <b-container>
+                    <b-row>
+                        <b-col cols="12">
+                            <h1 class="event-page__heading">
+                                Reserve our meeting rooms for your next event in Tashkent
+                            </h1>
+                            <div class="event-page__text-hide">
+                                With three outstanding meeting rooms that are designed to host all kinds of gatherings from small meetings to large receptions for up to 250 guests, the Radisson Blu is an ideal venue for your next event in Tashkent. We can arrange the furniture in our Grand Ballroom, Turquoise Room, and Sapphire Room in a variety of setups as per your requirement. Each room comes with modern audiovisual equipment, videoconferencing facilities, and high-speed wireless internet. Our dedicated Meetings & Events coordinator helps you plan every aspect of your event and ensure that everything runs smoothly and our experienced chefs offer customized menus for your guests. An on-site business center is also available for the meeting delegates.
+                            </div>
+                            <b-button class="event-page__text-btn">
+                                <span>Read more</span>
+                                <font-awesome-icon :icon="['fas', 'angle-down']" />
+                            </b-button>
+                        </b-col>
+                    </b-row>
+                </b-container>
+            </div>
+            <div class="event-page__contact">
+                <b-container>
+                    <b-row>
+                        <b-col cols="12">
+                            <h3 class="event-page__contact-title">
+                                Contact information
+                            </h3>
+                            <div class="event-page__contact-email">
+                                <font-awesome-icon :icon="['fas', 'envelope']" />
+                                <a href="mailto:reservation.tashkent@radissonblu.com">
+                                    reservation.tashkent@radissonblu.com
+                                </a>
+                            </div>                        
+                        </b-col>
+                    </b-row>
+                </b-container>
+            </div>
+            <div class="event-page__features">
+                <b-container>
+                    <b-row>
+                        <b-col md="12">
+                            <h2 class="event-page__features-title">
+                                Key features
+                            </h2>
+                        </b-col>
+                    </b-row>
+                    <b-row>
+                        <b-col sm="12">
+                            <ul class="event-page__features-list d-flex justufy-content-between">
+                                <li class="event-page__features-item">
+                                    <font-awesome-icon :icon="['fas', 'handshake']" />
+                                    <span>3 meeting rooms</span>
+                                </li>
+                                <li class="event-page__features-item">
+                                    <font-awesome-icon :icon="['fas', 'users']" />
+                                    <span>503 person max. capacity</span>
+                                </li>
+                                <li class="event-page__features-item">
+                                    <font-awesome-icon :icon="['fas', 'wifi']" />
+                                    <span>Catering service</span>
+                                </li>
+                                <li class="event-page__features-item">
+                                    <font-awesome-icon :icon="['fas', 'wifi']" />
+                                    <span>Free Wi-Fi</span>
+                                </li>
+                                <li class="event-page__features-item">
+                                    <font-awesome-icon :icon="['fas', 'parking']" />
+                                    <span>Complimentary parking</span>
+                                </li>
+                                <li class="event-page__features-item">
+                                    <font-awesome-icon :icon="['fas', 'print']" />
+                                    <span>Printing services</span>
+                                </li>
+                                <li class="event-page__features-item">
+                                    <font-awesome-icon :icon="['fas', 'chalkboard-teacher']" />
+                                    <span>Reception</span>
+                                </li>
+                                <li class="event-page__features-item">
+                                    <font-awesome-icon :icon="['fas', 'thermometer-quarter']" />
+                                    <span>Individual room climate control</span>
+                                </li>
+                            </ul>
+                        </b-col>
+                    </b-row>
+                </b-container>
+            </div>
+        </b-container>
+    </div>
+</template>
