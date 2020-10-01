ed66c84beb2abc681f0c0416fb11a6c440eb730b
Mirjalol9797 <miramirxomitov97@gmail.com>
2020-10-01 13:00:10 +0500

    finished events page and style

--- pages/events.vue
+++ pages/events.vue
@@ -11,7 +11,7 @@
                             <div class="event-page__text-hide">
                                 With three outstanding meeting rooms that are designed to host all kinds of gatherings from small meetings to large receptions for up to 250 guests, the Radisson Blu is an ideal venue for your next event in Tashkent. We can arrange the furniture in our Grand Ballroom, Turquoise Room, and Sapphire Room in a variety of setups as per your requirement. Each room comes with modern audiovisual equipment, videoconferencing facilities, and high-speed wireless internet. Our dedicated Meetings & Events coordinator helps you plan every aspect of your event and ensure that everything runs smoothly and our experienced chefs offer customized menus for your guests. An on-site business center is also available for the meeting delegates.
                             </div>
-                            <b-button class="event-page__text-btn">
+                            <b-button class="event-page__text-btn" variant="outline-light">
                                 <span>Read more</span>
                                 <font-awesome-icon :icon="['fas', 'angle-down']" />
                             </b-button>
@@ -57,7 +57,7 @@
                                     <span>503 person max. capacity</span>
                                 </li>
                                 <li class="event-page__features-item">
-                                    <font-awesome-icon :icon="['fas', 'wifi']" />
+                                    <font-awesome-icon :icon="['fas', 'hammer']" />
                                     <span>Catering service</span>
                                 </li>
                                 <li class="event-page__features-item">
@@ -85,6 +85,101 @@
                     </b-row>
                 </b-container>
             </div>
+            <div class="event-page__multimedia">
+                <b-container>
+                    <b-row>
+                        <b-col cols="12">
+                            <h2 class="event-page__multimedia-title">Meeting packages</h2>
+                        </b-col>
+                    </b-row>
+                    <b-row class="d-flex">
+                        <b-col lg="6">
+                            <b-img src="../assets/images/site_logo.jpg" fluid-grow alt="site_logo"></b-img>
+                        </b-col>
+                        <b-col lg="6">
+                            <h3 class="event-page__multimedia-heading">Custom tours for your team</h3>
+                            <h5 class="event-page__multimedia-subtitle">Chimgan and Charvak tours</h5>
+                            <p class="event-page__multimedia-text">Take your business meeting to a new level with a recreational trip to the Chimgan mountains and Charvak reservoir with your the entire team. Start the tour with a long and scenic drive to the Chimgan mountains. Take the cable car to Kumbel peak. Enjoy the spectacular views and even if you are not able to take the cable car, the picturesque scenery of the mountainous region is sure wow you. Continue to the shore of Charvak reservoir and enjoy a different kind of tranquil beauty. Enjoy sumptuous lunch at the Chinaras restaurant where you can even see rock carvings from prehistoric era.
+Price per person: USD 30* (may vary as per season)
+*Includes: Transportation, tour guide, and lunch</p>
+                            <b-button variant="outline-light" class="event-page__multimedia-btn">
+                                <span>Read more</span>
+                                <font-awesome-icon :icon="['fas', 'angle-down']" />
+                            </b-button>
+                        </b-col>
+                    </b-row>
+                </b-container>
+            </div>
         </b-container>
+        <div class="event-page__banner">
+            <b-container>
+                <b-row>
+                    <b-col lg="6">
+                        <div class="event-page__banner-text">
+                            <small>Meet your every objective with Radisson Meetings</small>
+                            <h3 class="event-page__banner-title">Discover Radisson Meetings</h3>
+                            <p>Our dedicated team, attention to detail, and outstanding facilities in destinations around the world ensure the success of your meetings and events.</p>
+                        </div>
+                        <div class="event-page__banner-link">
+                            <a href="/" target="_blank">Find out more</a>
+                        </div>
+                    </b-col>
+                </b-row>
+            </b-container>
+        </div>
+        <div class="event-page__footer">
+            <b-container>
+                <b-row>
+                    <b-col cols="12" class="event-page__footer-list">
+                        <b-col lg="4" class="event-page__footer-item">
+                            <div class="event-page__footer-title">Contact Radisson Hotels</div>
+                            <a href="/" target="_blank" class="event-page__footer-link">Need help?</a>
+                        </b-col>
+                        <b-col lg="4" class="event-page__footer-item">
+                            <div class="event-page__footer-title">Follow us on social media</div>
+                            <ul class="event-page__footer-social">
+                                <li class="event-page__footer-social-item">
+                                    <a href="/">
+                                        <font-awesome-icon :icon="['fab', 'facebook-f']" />
+                                    </a>
+                                </li>
+                                <li class="event-page__footer-social-item">
+                                    <a href="/">
+                                        <font-awesome-icon :icon="['fab', 'instagram']" />
+                                    </a>
+                                </li>
+                                <li class="event-page__footer-social-item">
+                                    <a href="/">
+                                        <font-awesome-icon :icon="['fab', 'pinterest']" />
+                                    </a>
+                                </li>
+                                <li class="event-page__footer-social-item">
+                                    <a href="/">
+                                        <font-awesome-icon :icon="['fab', 'twitter']" />
+                                    </a>
+                                </li>
+                                <li class="event-page__footer-social-item">
+                                    <a href="/">
+                                        <font-awesome-icon :icon="['fab', 'youtube']" />
+                                    </a>
+                                </li>
+                                <li class="event-page__footer-social-item">
+                                    <a href="/">
+                                        <font-awesome-icon :icon="['fab', 'linkedin-in']" />
+                                    </a>
+                                </li>
+                            </ul>
+                        </b-col>
+                        <b-col lg="4" class="event-page__footer-item">
+                            <div class="event-page__footer-title">Never miss out on our most popular deals</div>
+                            <b-form inline class="event-page__footer-form"> 
+                                <b-input type="email" placeholder="email address" required class="event-page__footer-input"></b-input>
+                                <b-button type="submit" variant="outline-light" class="event-page__footer-btn">Subscribe</b-button> 
+                            </b-form>
+                        </b-col>
+                    </b-col>
+                </b-row>
+            </b-container>
+        </div>
     </div>
 </template>
