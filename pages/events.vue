7e3340bba69752cd1a71feca705136d1a613ceb0
Mirjalol9797 <miramirxomitov97@gmail.com>
2020-10-02 14:19:47 +0500

    merged

--- pages/events.vue
+++ pages/events.vue
@@ -9,7 +9,7 @@
                                 Reserve our meeting rooms for your next event in Tashkent
                             </h1>
                             <div class="event-page__text-hide" :class="{autoheight:isActive}">
-                                With three outstanding meeting rooms that are designed to host all kinds of gatherings from small meetings to large receptions for up to 250 guests, the Radisson Blu is an ideal venue for your next event in Tashkent. We can arrange the furniture in our Grand Ballroom, Turquoise Room, and Sapphire Room in a variety of setups as per your requirement. Each room comes with modern audiovisual equipment, videoconferencing facilities, and high-speed wireless internet. Our dedicated Meetings & Events coordinator helps you plan every aspect of your event and ensure that everything runs smoothly and our experienced chefs offer customized menus for your guests. An on-site business center is also available for the meeting delegates.
+                                With three outstanding meeting rooms that are designed to host all kinds of gatherings from small meetings to large receptions for up to 250 guests, the Sunrine Blu is an ideal venue for your next event in Tashkent. We can arrange the furniture in our Grand Ballroom, Turquoise Room, and Sapphire Room in a variety of setups as per your requirement. Each room comes with modern audiovisual equipment, videoconferencing facilities, and high-speed wireless internet. Our dedicated Meetings & Events coordinator helps you plan every aspect of your event and ensure that everything runs smoothly and our experienced chefs offer customized menus for your guests. An on-site business center is also available for the meeting delegates.
                             </div>
                             <b-button class="event-page__text-btn" variant="outline-light">
                                 <span @click="isActive = !isActive" v-if="isActive === false">
@@ -35,7 +35,7 @@
                             <div class="event-page__contact-email">
                                 <font-awesome-icon :icon="['fas', 'envelope']" />
                                 <a href="mailto:reservation.tashkent@radissonblu.com">
-                                    reservation.tashkent@radissonblu.com
+                                    reservation.tashkent@Sunrise.com
                                 </a>
                             </div>                        
                         </b-col>
@@ -100,15 +100,16 @@
                     </b-row>
                     <b-row class="d-flex">
                         <b-col lg="6">
-                            <b-img src="../assets/images/site_logo.jpg" fluid-grow alt="site_logo"></b-img>
+                            <b-img src="../assets/images/site_logo.jpg" fluid-grow rounded mx-auto d-block alt="site_logo" style="width:450px !important"></b-img>
                         </b-col>
                         <b-col lg="6">
                             <h3 class="event-page__multimedia-heading">Custom tours for your team</h3>
                             <h5 class="event-page__multimedia-subtitle">Chimgan and Charvak tours</h5>
-                            <p class="event-page__multimedia-text" :class="{autoheight:isActive}">Take your business meeting to a new level with a recreational trip to the Chimgan mountains and Charvak reservoir with your the entire team. Start the tour with a long and scenic drive to the Chimgan mountains. Take the cable car to Kumbel peak. Enjoy the spectacular views and even if you are not able to take the cable car, the picturesque scenery of the mountainous region is sure wow you. Continue to the shore of Charvak reservoir and enjoy a different kind of tranquil beauty. Enjoy sumptuous lunch at the Chinaras restaurant where you can even see rock carvings from prehistoric era.
+                            <p class="event-page__multimedia-text" :class="{autoheight:isActive}" >
+                                Take your business meeting to a new level with a recreational trip to the Chimgan mountains and Charvak reservoir with your the entire team. Start the tour with a long and scenic drive to the Chimgan mountains. Take the cable car to Kumbel peak. Enjoy the spectacular views and even if you are not able to take the cable car, the picturesque scenery of the mountainous region is sure wow you. Continue to the shore of Charvak reservoir and enjoy a different kind of tranquil beauty. Enjoy sumptuous lunch at the Chinaras restaurant where you can even see rock carvings from prehistoric era.
 Price per person: USD 30* (may vary as per season)
 *Includes: Transportation, tour guide, and lunch</p>
-                            <b-button variant="outline-light" class="event-page__multimedia-btn">
+                            <b-button variant="outline-light" class="event-page__multimedia-btn" >
                                 <span @click="isActive = !isActive" v-if="isActive === false">
                                     Read more
                                     <font-awesome-icon :icon="['fas', 'angle-down']" />
@@ -129,8 +130,8 @@ Price per person: USD 30* (may vary as per season)
                 <b-row>
                     <b-col lg="6">
                         <div class="event-page__banner-text">
-                            <small>Meet your every objective with Radisson Meetings</small>
-                            <h3 class="event-page__banner-title">Discover Radisson Meetings</h3>
+                            <small>Meet your every objective with Sunrise Meetings</small>
+                            <h3 class="event-page__banner-title">Discover Sunrise Meetings</h3>
                             <p>Our dedicated team, attention to detail, and outstanding facilities in destinations around the world ensure the success of your meetings and events.</p>
                         </div>
                         <div class="event-page__banner-link">
@@ -145,7 +146,7 @@ Price per person: USD 30* (may vary as per season)
                 <b-row>
                     <b-col cols="12" class="event-page__footer-list">
                         <b-col lg="4" class="event-page__footer-item">
-                            <div class="event-page__footer-title">Contact Radisson Hotels</div>
+                            <div class="event-page__footer-title">Sunrise Hotels</div>
                             <a href="/" target="_blank" class="event-page__footer-link">Need help?</a>
                         </b-col>
                         <b-col lg="4" class="event-page__footer-item">
