b19fa72cc70866331e8afec6b684eb585dcc8392
Mirjalol9797 <miramirxomitov97@gmail.com>
2020-10-09 10:06:24 +0500

    finished offer page

--- pages/offer.vue
+++ pages/offer.vue
@@ -2,12 +2,12 @@
     <div class="offer-page">
         <b-container>
             <b-row class="offer-page__wrap">
-                <b-col lg="5">
+                <b-col md="5">
                     <div class="offer-page__img">
                         <b-img src="../assets/images/offer_img.jpg" alt=""></b-img>
                     </div>
                 </b-col>
-                <b-col lg="7">
+                <b-col md="7">
                     <div class="offer-page__info">
                         <div class="offer-page__info-title">Change of Seasons. Change of Scenery.</div>
                         <div class="offer-page__info-subtitle">
@@ -19,9 +19,19 @@
                             <b-link href="/" class="offer-page__link">Register Now</b-link>
                         </div>
                         <div class="offer-page__text">
-                            <span v-b-toggle.collapse >
+                            <span v-b-toggle.collapse 
+                                @click="show = !show"
+                                v-if="show === false"
+                            >
                                 TERMS AND CONDITIONS
-                                <font-awesome-icon :icon="['fas', 'angle-down']" />
+                                <font-awesome-icon :icon="['fas', 'chevron-down']" />
+                            </span>
+                            <span v-b-toggle.collapse 
+                                v-else
+                                @click="show = !show"
+                            >
+                                TERMS AND CONDITIONS
+                                <font-awesome-icon :icon="['fas', 'chevron-up']" />
                             </span>
                             <b-collapse id="collapse">
                                 <b-card>
@@ -44,6 +54,126 @@
                     </div>
                 </b-col>
             </b-row>
+            <b-row class="offer-page__wrap">
+                <b-col md="5">
+                    <div class="offer-page__img">
+                        <b-img src="../assets/images/offer_img.jpg" alt=""></b-img>
+                    </div>
+                </b-col>
+                <b-col md="7">
+                    <div class="offer-page__info">
+                        <div class="offer-page__info-title">Change of Seasons. Change of Scenery.</div>
+                        <div class="offer-page__info-subtitle">
+                            Whatever new view you choose, Bonus Journeys is back with up to 4x points and double
+                            nights toward tier status through January 4, 2021.
+                        </div>
+                        <div class="offer-page__registr">
+                            <span>Registration required.</span>
+                            <b-link href="/" class="offer-page__link">Register Now</b-link>
+                        </div>
+                        <div class="offer-page__text">
+                            <span v-b-toggle.collapse 
+                                @click="show = !show"
+                                v-if="show === false"
+                            >
+                                TERMS AND CONDITIONS
+                                <font-awesome-icon :icon="['fas', 'chevron-down']" />
+                            </span>
+                            <span v-b-toggle.collapse 
+                                v-else
+                                @click="show = !show"
+                            >
+                                TERMS AND CONDITIONS
+                                <font-awesome-icon :icon="['fas', 'chevron-up']" />
+                            </span>
+                            <b-collapse id="collapse">
+                                <b-card>
+                                    Triple Points Bonus: You must be a member of World of Hyatt and register for 
+                                    the promotion between October 1, 2020 and November 30, 2020 to participate.
+                                    To join World of Hyatt, visit worldofhyatt.com, and to register for the promotion,
+                                    visit hyatt.com/bonusjourneys. All eligible stays under this promotion must 
+                                    be completed October 1, 2020 through January 4, 2021 (“Promotion Period”). 
+                                    Beginning on your first eligible stay after registration, you will receive 
+                                    Triple Points on eligible spend for stays at Hyatt hotels and resorts worldwide
+                                    and participating M life Rewards destinations, Small Luxury Hotels of the World
+                                    (“SLH”) properties, and Lindblad Expeditions. Maximum 150,000 Bonus Points 
+                                    may be earned for the Triple Points Bonus during Promotion Period.
+                                </b-card>
+                            </b-collapse>
+                        </div>
+                        <div class="offer-page__check">
+                            <b-button variant="outline-light" class="offer-page__btn">Check Availability</b-button>
+                        </div>
+                    </div>
+                </b-col>
+            </b-row>    
+            <b-row class="offer-page__wrap">
+                <b-col md="5">
+                    <div class="offer-page__img">
+                        <b-img src="../assets/images/offer_img.jpg" alt=""></b-img>
+                    </div>
+                </b-col>
+                <b-col md="7">
+                    <div class="offer-page__info">
+                        <div class="offer-page__info-title">Change of Seasons. Change of Scenery.</div>
+                        <div class="offer-page__info-subtitle">
+                            Whatever new view you choose, Bonus Journeys is back with up to 4x points and double
+                            nights toward tier status through January 4, 2021.
+                        </div>
+                        <div class="offer-page__registr">
+                            <span>Registration required.</span>
+                            <b-link href="/" class="offer-page__link">Register Now</b-link>
+                        </div>
+                        <div class="offer-page__text">
+                            <span v-b-toggle.collapse 
+                                @click="show = !show"
+                                v-if="show === false"
+                            >
+                                TERMS AND CONDITIONS
+                                <font-awesome-icon :icon="['fas', 'chevron-down']" />
+                            </span>
+                            <span v-b-toggle.collapse 
+                                v-else
+                                @click="show = !show"
+                            >
+                                TERMS AND CONDITIONS
+                                <font-awesome-icon :icon="['fas', 'chevron-up']" />
+                            </span>
+                            <b-collapse id="collapse">
+                                <b-card>
+                                    Triple Points Bonus: You must be a member of World of Hyatt and register for 
+                                    the promotion between October 1, 2020 and November 30, 2020 to participate.
+                                    To join World of Hyatt, visit worldofhyatt.com, and to register for the promotion,
+                                    visit hyatt.com/bonusjourneys. All eligible stays under this promotion must 
+                                    be completed October 1, 2020 through January 4, 2021 (“Promotion Period”). 
+                                    Beginning on your first eligible stay after registration, you will receive 
+                                    Triple Points on eligible spend for stays at Hyatt hotels and resorts worldwide
+                                    and participating M life Rewards destinations, Small Luxury Hotels of the World
+                                    (“SLH”) properties, and Lindblad Expeditions. Maximum 150,000 Bonus Points 
+                                    may be earned for the Triple Points Bonus during Promotion Period.
+                                </b-card>
+                            </b-collapse>
+                        </div>
+                        <div class="offer-page__check">
+                            <b-button variant="outline-light" class="offer-page__btn">Check Availability</b-button>
+                        </div>
+                    </div>
+                </b-col>
+            </b-row>                     
         </b-container>
     </div>
 </template>
+<script>
+    export default {
+        data() {
+            return {
+                show: false,
+
+            }
+        },
+        methods: {
+
+        }
+    }
+
+</script>
