dba03ffaa1e5de25fe26ad12736c3939a31c8dd5
Hojiakbar88 <hojiakbaregamberdiyev88@gmail.com>
2020-10-14 17:03:50 +0500

    user page wip

---
+++ pages/my-account.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="account">
+    <div class="account__header">
+      <b-col class="account__title" lg="12">
+        <h2>Welcome back, User Name</h2>
+        <button class="btn btn-outline-warning" type="button">BOOK NOW</button>
+      </b-col>
+    </div>
+    <b-container>
+      <b-row>
+        <b-col class="account__content" lg="12">
+
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
