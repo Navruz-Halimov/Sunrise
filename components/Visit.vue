42b22750ec8114e944e299267c6b2653a2ca750e
Hojiakbar88 <hojiakbaregamberdiyev88@gmail.com>
2020-10-17 17:25:08 +0500

    change visit

--- components/Visit.vue
+++ components/Visit.vue
@@ -13,7 +13,7 @@
               <h6>{{image.title}}</h6>
               <span class="like" @click.stop="">
                 <button type="button" @click="likeBtn(image.id)">
-                <svg v-bind:class="{ 'like__icon--active': image.id === likeID}" class="like__icon" viewBox="0 0 18 15" fill="currentColor" width="21" height="21">
+                <svg v-bind:class="{ 'like__icon--active': image.id === likeID }" class="like__icon" viewBox="0 0 18 15" fill="currentColor" width="21" height="21">
                   <path fill="currentColor" d="M9,15c-0.1,0-0.2,0-0.3-0.1c-0.2-0.2-6-3.9-7.8-7C0,6.3-0.3,4.5,0.3,3C0.8,1.7,1.7,0.7,3,0.3	C5.3-0.5,7.7,0.7,9,2.4c1.4-1.9,4-2.9,6.1-2.1c1.2,0.5,2.2,1.4,2.6,2.6c0.6,1.5,0.3,3.3-0.6,5c-1.8,3.3-7.5,6.8-7.8,7	C9.2,15,9.1,15,9,15z"></path>
                 </svg>
                 </button>
