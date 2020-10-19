d5bd2b6edace49f94a99ea782eb0bf5a63161fa4
Navruz <navruz.halimov.3999@gmail.com>
2020-10-19 03:18:54 -0700

    responsive rooms__select page

--- pages/rooms-inner.vue
+++ pages/rooms-inner.vue
@@ -2,36 +2,44 @@
   <div class="rooms__select">
     <b-container>
       <b-row>
-        <b-col lg="3">
+        <b-col lg="3" class="rooms__select-left">
           <ul class="rooms__select-amenties">
             <span class="rooms__select-amenties-heading">Amenties</span>
-            <li class="rooms__select-amenties-item">Regency Club access</li>
-            <li class="rooms__select-amenties-item">Espresso machine</li>
-            <li class="rooms__select-amenties-item">Hyatt Grand Bed™</li>
-            <li class="rooms__select-amenties-item">55-inch flatscreen TV</li>
-            <li class="rooms__select-amenties-item">Blackout drapes</li>
-            <li class="rooms__select-amenties-item">Spacious work desk</li>
-            <li class="rooms__select-amenties-item">
-              Individually controlled air conditioning and heat
-            </li>
-            <li class="rooms__select-amenties-item">TV in bathroom</li>
-            <li class="rooms__select-amenties-item">In-room safe</li>
-            <li class="rooms__select-amenties-item">Minibar</li>
-            <li class="rooms__select-amenties-item">Tea and coffeemaker</li>
-            <li class="rooms__select-amenties-item">Iron and ironing board</li>
-            <li class="rooms__select-amenties-item">Hair dryer</li>
-            <li class="rooms__select-amenties-item">
-              Plush bath robe and slippers
-            </li>
-            <li class="rooms__select-amenties-item">Morning newspaper</li>
-            <li class="rooms__select-amenties-item">Pillow menu</li>
-            <li class="rooms__select-amenties-item">Crib upon request</li>
-            <li class="rooms__select-amenties-item">
-              Rollaway bed upon request
-            </li>
+            <b-button v-b-toggle.collapse-1 class="rooms__select-amenties-heading mobil">
+              Amenties
+            </b-button>
+            <b-collapse id="collapse-1">
+              <b-card>
+                <li class="rooms__select-amenties-item">Regency Club access</li>
+                <li class="rooms__select-amenties-item">Espresso machine</li>
+                <li class="rooms__select-amenties-item">Hyatt Grand Bed™</li>
+                <li class="rooms__select-amenties-item">55-inch flatscreen TV</li>
+                <li class="rooms__select-amenties-item">Blackout drapes</li>
+                <li class="rooms__select-amenties-item">Spacious work desk</li>
+                <li class="rooms__select-amenties-item">
+                  Individually controlled air conditioning and heat
+                </li>
+                <li class="rooms__select-amenties-item">TV in bathroom</li>
+                <li class="rooms__select-amenties-item">In-room safe</li>
+                <li class="rooms__select-amenties-item">Minibar</li>
+                <li class="rooms__select-amenties-item">Tea and coffeemaker</li>
+                <li class="rooms__select-amenties-item">Iron and ironing board</li>
+                <li class="rooms__select-amenties-item">Hair dryer</li>
+                <li class="rooms__select-amenties-item">
+                  Plush bath robe and slippers
+                </li>
+                <li class="rooms__select-amenties-item">Morning newspaper</li>
+                <li class="rooms__select-amenties-item">Pillow menu</li>
+                <li class="rooms__select-amenties-item">Crib upon request</li>
+                <li class="rooms__select-amenties-item">
+                  Rollaway bed upon request
+                </li>                
+              </b-card>
+            </b-collapse>
+            
           </ul>
         </b-col>
-        <b-col lg="6">
+        <b-col lg="6" class="rooms__select-center">
           <swiper class="rooms__select-slider" :options="rooms__select">
             <swiper-slide
               ><img src="../assets//images/3D/1.jpg" alt="" />
@@ -55,20 +63,65 @@
             premium suite.
           </div>
         </b-col>
-        <b-col lg="3">
+        <b-col lg="3" class="rooms__select-right">
           <div class="floor__left-wrapper">
-          <div class="floor__number">First Floor scheme</div>
-          <img class="floor__mini-2d" src="../assets/images/1floor/1.jpg" alt="">
+            <!-- <div class="floor__number">First Floor scheme</div>
+            <img class="floor__mini-2d" src="../assets/images/1floor/1.jpg" alt=""> -->
+            <form class="booking__form" method="" action="POST">
+                <section class="order__part">
+                  <b-row>
+                    <div class="col-lg-12 order__part-item">
+                      <date-picker
+                        v-model="value2"
+                        value-type="format"
+                        format="YYYY-MM-DD"
+                        :placeholder="$t('booking.arrival')"
+                        :default-value="new Date()"
+                        :disabled-date="notBeforeToday"
+                      ></date-picker>
+                    </div>
+                    <div class="col-lg-12 order__part-item">
+                      <date-picker
+                        v-model="value3"
+                        value-type="format"
+                        format="YYYY-MM-DD"
+                        :placeholder="$t('booking.departure')"
+                        :default-value="new Date()"
+                        :disabled-date="notBeforeToday"
+                      ></date-picker>
+                    </div>
+                    <div class="col-lg-12 order__part-item">
+                      <select name="adults__capacity" class="adults__capacity">
+                        <option value="0" selected disabled>{{$t('booking.adults_title')}}</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                      </select>
+                    </div>
+                    <div class="col-lg-12 order__part-item">
+                      <select name="children__capacity" class="children__capacity">
+                        <option value="0" selected disabled>{{$t('booking.children_title')}}</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                      </select>
+                    </div>
+                  </b-row>
+                </section>
+            </form>
             <p class="floor__text">First floor rooms</p>
-          <b-pagination-nav
-            number-of-pages="10"
-            base-url="#"
-            hide-goto-end-buttons = "false"
-            pills = "true"
-            limit="10"
-            class="room__inner-pagination"
-          ></b-pagination-nav>
-            <b-button class="floor__btn" pill :to="localePath('/payment')">Select room</b-button>
+            <b-pagination-nav
+              number-of-pages="10"
+              base-url="#"
+              hide-goto-end-buttons = "false"
+              pills = "true"
+              limit="10"
+              class="room__inner-pagination"
+            ></b-pagination-nav>
+              <b-button class="floor__btn" pill :to="localePath('/payment')">Select room</b-button>
+              
           </div>
         </b-col>
       </b-row>
@@ -80,6 +133,8 @@
 export default {
   data() {
     return {
+      value2: [new Date()],
+      value3: [new Date()],
       rooms__select: {
         centeredSlides: true,
         spaceBetween: 30,
@@ -94,6 +149,10 @@ export default {
       },
     }
   },
-  methods: {},
+  methods: {
+    notBeforeToday(date) {
+      return date < new Date(new Date().setHours(0, 0, 0, 0))
+    },    
+  },
 }
 </script>
